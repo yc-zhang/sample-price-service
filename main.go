1ec62fa3d91f21e21e671b08656db8fee949c4c4
yc-zhang <turalyon.zhangyc@gmail.com>
2018-09-03 23:52:39 +0800

    get the fruit values from redis.

--- main.go
+++ main.go
@@ -2,35 +2,52 @@ package main
 
 import (
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"strings"
 )
 
 var DB = make(map[string]string)
+var redisClient = redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_SERVER")})
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := DB[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+	r.GET("/fruit", func(c *gin.Context) {
+		namesQuery := c.Query("names")
+		prices := make(map[string]string)
+		if namesQuery != "" {
+			names := strings.Split(namesQuery, ",")
+			for _, name := range names {
+				prices[name] = getPrice(name)
+			}
 		}
+		c.JSON(http.StatusOK, prices)
 	})
 
 	return r
 }
 
+func initRedis()  {
+	redisClient.Set("apple", "10", 0)
+	redisClient.Set("orange", "5", 0)
+	redisClient.Set("grape", "1", 0)
+	redisClient.Set("pineapple", "15", 0)
+	redisClient.Set("durian", "20", 0)
+}
+
+func getPrice(fruit string) string {
+	val, err := redisClient.Get(fruit).Result()
+	if err == redis.Nil || err != nil {
+		return "N/A"
+	} else {
+		return val
+	}
+}
+
 func main() {
+	initRedis()
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
